cb316b74ddbb33dad60cca9114d20231650f1bb6
Benjamín Silva <silva96@gmail.com>
2020-04-25 19:58:06 -0400

    add colors to timers in js

--- app/javascript/controllers/character_controller.js
+++ app/javascript/controllers/character_controller.js
@@ -23,6 +23,7 @@ export default class extends Controller {
 
   amped(secondsLeft) {
     clearInterval(this.ampTimer)
+    this.paintClass(secondsLeft)
     if (secondsLeft < 0) {
       this.ampTarget.innerText = '--'
     }
@@ -32,11 +33,25 @@ export default class extends Controller {
     this.ampTimer = setInterval(()=>{
       secondsLeft -= 1
       this.ampTarget.innerText = secondsLeft.toString()
+
       if (secondsLeft < 0) {
         clearInterval(this.ampTimer)
         this.ampTarget.innerText = '--'
       }
     }, 1000)
+  }
+
+  paintClass(secondsLeft) {
+    const time = this.ampTarget.closest('.time')
+    time.classList.remove('has-text-success', 'has-text-danger', 'has-text-warning')
+    if(secondsLeft < 5) {
+      time.classList.add('has-text-danger')
+    } else if (secondsLeft < 15) {
+      time.classList.add('has-text-warning')
+    }
+    else {
+      time.classList.add('has-text-success')
+    }
   }
 
   clearIntervals() {
