f965c7f250fc95fa3e92e4194ff8c6af1ebecb44
Benjamín Silva <silva96@gmail.com>
2020-04-27 11:57:28 -0400

    small bugfix

--- app/reflexes/room_reflex.rb
+++ app/reflexes/room_reflex.rb
@@ -10,7 +10,7 @@ class RoomReflex < ApplicationReflex
     enemy = data['enemy']
     name = data['name']
     room_id = data['room_id']
-    character = Character.create_with(room_id: room_id, enemy: enemy)
+    character = Character.create_with(enemy: enemy)
                          .find_or_create_by(name: name, room_id: room_id)
     character.update(
       amp_timed_at: 61.seconds.ago,
